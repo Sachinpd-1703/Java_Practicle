bb5686f7e7baea43fb15514a9de5f7137e752bb2
Sachin Prasad <147019955+Sachinpd-1703@users.noreply.github.com>
2024-07-08 18:59:27 +0530

    Update Practicle_01.java

--- Practicle_01.java
+++ Practicle_01.java
@@ -1,3 +1,7 @@
+// write a program to display Hello World message in console window.
+// use Command promt to run in console window
+// type   javac <file_name>
+// then   java <file_name>
 public class Practicle_01{
     public static void main(String[] args){
         System.out.println("Hello World");
